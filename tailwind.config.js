4abc4357936f426be8de34548be45e6611decd66
Jason <jasonetienne1@gmail.com>
2023-08-15 20:02:51 +0100

    added a simple button

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'], // Add the path to your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
