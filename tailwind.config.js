2e1fe299f4658bd6c8ccc250a0bd8ca336ae4f24
Jason <jasonetienne1@gmail.com>
2023-08-15 20:06:29 +0100

    added mode, purge and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index'],
+  media: true,
   content: ['./**/*.html'], // Add the path to your HTML files
   theme: {
     extend: {},
